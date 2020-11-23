4fae69a7aef270be1de7a69bd658b4ec014c6d72
unknown <eportillovirreira@gmail.com>
2020-11-23 17:34:15 -0400

    primer commit y metodo registrar clientes

---
+++ src/controllers/index.controller.js
@@ -0,0 +1,25 @@
+const { pool, Pool } = require('pg');
+const { json } = require('express');
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    password: '8450706183',
+    database: 'hotel_proyecto_final_bd',
+    port: '5432' 
+});
+
+
+// PA registrar_clientes
+
+const registrar_cliente = async(req, res) => {
+    const {nombre, apellido, telefono, email, direccion, pais} = req.body;
+
+    const response = await pool.query('SELECT registrar_cliente($1, $2, $3, $4, $4, $5)', 
+    [nombre, apellido, telefono, email, direccion, pais]);
+
+    return res.status().json({
+        message: '', 
+        body: {nombre, apellido, email} 
+    });
+};
