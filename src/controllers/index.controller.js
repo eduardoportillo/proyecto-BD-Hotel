504669d7bea8ef04851ceba8396abc0fbc495cce
unknown <eportillovirreira@gmail.com>
2020-11-29 10:08:13 -0400

    agrege los paises al datalist y comence a hacer reserva habitacion todo ok

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -15,11 +15,15 @@ const index = async (req, res) => {
 // metodos para tabla clientes
 const getClientes = async (req, res) => {
     const query = 'SELECT * FROM clientes';
+    const paises = await pool.query('SELECT pais from nacionalidades');
     const response = pool.query(query, (error, result) => {
         if (error) {
             throw error;
         }
-        res.render('registrar_cliente', { date_clientes: result.rows });
+        res.render('registrar_cliente', { 
+            date_clientes: result.rows,
+            data_paises: paises.rows
+        });
     });
 };
 
@@ -31,15 +35,26 @@ const registrarCliente = async(req, res) => {
 };
 
 const deleteCliente = async (req, res) => {
-    // const id = parseInt(req.params.id);
     const { id } = req.params;
     await pool.query('DELETE FROM clientes where cliente_id = $1', [id]);
     res.redirect('/cliente');
+};
+
+// metodo para tabla reserva_habitacion
+
+const reserva_habitacion = async (req, res) => {
+    const res_cliente = await pool.query('SELECT * FROM clientes');
+    const res_habitacion = await pool.query('SELECT * FROM reservas_habitaciones')
+    res.render('registrar_reservas', {
+        data_cliente: res_cliente.rows,
+        data_habitaciones: res_habitacion.rows
+    })
 };
 
 module.exports = {
     index,
     getClientes,
     registrarCliente,
-    deleteCliente
+    deleteCliente,
+    reserva_habitacion
 };
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,11 +1,13 @@
 const { Router } = require('express');
 const router = Router();
 
-const { index, getClientes, registrarCliente, deleteCliente} = require('../controllers/index.controller');
+const { index, getClientes, registrarCliente, deleteCliente, reserva_habitacion} = require('../controllers/index.controller');
 
 router.get('/', index);
 router.get('/cliente', getClientes);
 router.post('/registrar_cliente', registrarCliente);
 router.get('/delete/:id', deleteCliente);
+
+router.get('/reserva_habitaciones', reserva_habitacion);
 
 module.exports = router;
